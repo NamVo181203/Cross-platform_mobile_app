e8dc750188201b15111366647362649860ebd425
NamVo <92841674+NamVo181203@users.noreply.github.com>
2024-08-27 18:32:14 +0700

    Add files via upload

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,12 +8,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:lab1_i_am_rich/main.dart';
+import 'package:namvv_lab2/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MaterialApp(home: Text("Hello World")));
+    await tester.pumpWidget(const MyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
