cb9c97106785c6ee85f52cfba2c8f74372d57f28
NamVo <92841674+NamVo181203@users.noreply.github.com>
2024-08-27 18:31:24 +0700

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,91 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.blueGrey,
-        appBar: AppBar(
-          title: const Text(
-              'I Am Rich',
-            style: TextStyle(
-              color: Colors.white
-            ),
-          ),
-          backgroundColor: Colors.blueGrey[900],
-        ),
-        body: const Center(
-          child: Image(
-            image: AssetImage('images/diamond.png'),
-          ),
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const CircleAvatar(
+                  radius: 50.0,
+                  backgroundImage: AssetImage('images/profile.jpg'),
+                ),
+                const Text(
+                  'Vo Van Nam',
+                  style: TextStyle(
+                    fontSize: 35.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Pacifico',
+                  ),
+                ),
+                const SizedBox(height: 5.0,),
+                Text(
+                  'FLUTTER DEVELOPER',
+                  style: TextStyle(
+                      fontSize: 15.0,
+                      color: Colors.teal.shade100,
+                      fontFamily: 'Source Sans Pro',
+                      letterSpacing: 2.5,
+                      fontWeight: FontWeight.bold
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                  width: 150.0,
+                  child: Divider(
+                    color: Colors.teal.shade100,
+                  ),
+                ),
+                Card(
+                  margin: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading: const Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      '+84 935 193 055',
+                      style: TextStyle(
+                          color: Colors.teal.shade900,
+                          fontFamily: 'Source Sans Pro',
+                          fontSize: 15.0
+                      ),
+                    ),
+                  ),
+                ),
+                Card(
+                    margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.5),
+                    child: ListTile(
+                      leading: const Icon(
+                        Icons.email,
+                        color: Colors.teal,
+                      ),
+                      title: Text(
+                        'namvv.21it@vku.udn.vn',
+                        style: TextStyle(
+                            fontSize: 15.0,
+                            color: Colors.teal.shade900,
+                            fontFamily: 'Source Sans Pro'
+                        ),
+                      ),
+                    )
+                )
+              ],
+            )
         ),
       ),
-    ),
-  );
+      debugShowCheckedModeBanner: false,
+    );
+  }
 }
